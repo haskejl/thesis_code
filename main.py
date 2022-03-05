0f87eb8405f6ea086ebd0367ab241942e0ec3fc7
Joseph Haske <joseph.l.haske@gmail.com>
2022-03-05 14:34:30 +0200

    Added calculation for range of p

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def main_quadtree_one_strike():
         sys.stdout.flush()
 
         Y_bar = est.use_cdf(cdf_Ybar[0], cdf_Ybar[1], N)
+        #Y_bar = np.repeat([np.log(0.234)], N)
         res[run] = qt.calc_quad_tree_ev(x0, Y_bar, N, strike, qt.payoff_func_call, p)
     
     bs_price = bsf.bs_call(s0, strike, 42/252, 0.0343, 0.234, 0)
@@ -40,21 +41,25 @@ def main_quadtree_all_strikes():
     N = 100
     #strikes = np.array([70, 75]) #smaller array for testing
     strikes = np.array([60, 70, 75, 80, 85, 90, 95])
-    p = 0.14
+    p = np.linspace(1/12, 1/6, 20)
     #X_vals = np.array([83.94,81.68]) #HL Jul 18, 2005
     X_vals = np.log(np.array([78.38,78.21])) #OC Jul 18, 2005 
     s0 = 83.7 # Price given on p. 36
     x0 = np.log(s0) 
-    res = np.zeros((nruns, len(strikes)))
+    res = np.zeros((nruns, len(strikes), len(p)))
     print("Calculating cdf...")
     cdf_Ybar = est.calc_cdf(X_vals)
+    t_len = len(strikes)*len(p)
 
     for run in range(0,nruns):
         for strike in range(0, len(strikes)):
-            sys.stdout.write("\rPerforming run " + str(run+1) + " of " + str(nruns) + " [" + u"\u25A0"*strike + " "*(len(strikes)-strike-1) + "]")
-            sys.stdout.flush()
-            Y_bar = est.use_cdf(cdf_Ybar[0], cdf_Ybar[1], N)
-            res[run,strike] = qt.calc_quad_tree_ev(x0, Y_bar, N, strikes[strike], qt.payoff_func_call, p)
+            for p_val in range(0, len(p)):
+                progress = int(np.floor((strike*len(p) + p_val)/t_len*50))
+                blanks = 50-progress
+                sys.stdout.write("\rPerforming run " + str(run+1) + " of " + str(nruns) + " [" + u"\u25A0"*progress + " "*blanks + "]")
+                sys.stdout.flush()
+                Y_bar = est.use_cdf(cdf_Ybar[0], cdf_Ybar[1], N)
+                res[run,strike,p_val] = qt.calc_quad_tree_ev(x0, Y_bar, N, strikes[strike], qt.payoff_func_call, p[p_val])
         #print()
     
     bs_price = bsf.bs_call(s0, strikes, 42/252, 0.0343, 0.234, 0)
@@ -62,6 +67,7 @@ def main_quadtree_all_strikes():
     print("\n")
     for i in range(0,len(strikes)):
         print("For strike ", strikes[i], ":")
+        print("                    p: = ", p)
         print("Overall EV for strike: = ", np.mean(res[:,i]))
         print("BS Price for assumptions = ", bs_price[i])
         print()
