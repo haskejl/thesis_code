765393f8fc3757de867b884febed08a4ae7e8ac5
Joseph Haske <joseph.l.haske@gmail.com>
2022-04-08 17:39:11 +0300

    changed to try s&p 500

--- estimdist.py
+++ estimdist.py
@@ -1,10 +1,15 @@
 import numpy as np
 
 #alpha, beta, mu, and nu are defined on page 25 from the IBM specific data
-alpha = 11.85566
-beta = 4.13415
-mu = 0.04588
-nu = 0.9345938
+#alpha = 11.85566
+#beta = 4.13415
+#mu = 0.04588
+#nu = 0.9345938
+alpha = 50.0
+beta = 1.0
+nu = -4.38
+mu = 0.04
+
 
 #phi as defined on page 10
 def calc_phi(x):
@@ -28,8 +33,8 @@ def mutation_step(X, Y, n):
     
     X_prime = np.zeros(n)
     Y_prime = np.zeros(n)
-    X_prime[:] = X
-    Y_prime[:] = Y
+    X_prime = X
+    Y_prime = Y
     
     #Formula 3.2
     u = np.random.normal(size = (M,n))
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import comb_s_v_tree as svt
 def main_quadtree_one_strike():
     nruns = 100
     N = 100
-    strike = 70
+    strike = 1180 #70
     p = 0.135
     # 3 months of data
     #data = open("./data/IBM_2005_18May_to_18Jul.csv")
@@ -21,12 +21,21 @@ def main_quadtree_one_strike():
     #X_vals = np.zeros(len(lines))
     #for i in range(0,len(lines)):
         #X_vals[i] = np.log(float(lines[i].split(",")[1].strip()))
+
+    data = open("./data/sp500.csv")
+    lines = data.readlines()
+    data.close()
+
+    X_vals = np.zeros(len(lines))
+    for i in range(0,len(lines)):
+        X_vals[i] = np.log(float(lines[i].strip()))
     #X_vals = np.log(np.array([78.38,78.21]))
-    X_vals = np.log(np.array([76.36,77.16,76.41,76.51,75.81,76.0,77.14,77.1,75.55,76.84,77.35,75.79,75.0,75.04,74.8,
-        74.93,74.77,75.05,74.89,76.3,77.05,76.39,76.55,76.41,77.23,75.41,74.01,73.88,75.3,074.73,
-        74.2,74.67,74.79,75.81,77.38,79.3,78.96,80.04,81.45,82.42,82.38,81.81]))
+    #X_vals = np.log(np.array([76.36,77.16,76.41,76.51,75.81,76.0,77.14,77.1,75.55,76.84,77.35,75.79,75.0,75.04,74.8,
+    #    74.93,74.77,75.05,74.89,76.3,77.05,76.39,76.55,76.41,77.23,75.41,74.01,73.88,75.3,074.73,
+    #    74.2,74.67,74.79,75.81,77.38,79.3,78.96,80.04,81.45,82.42,82.38,81.81]))
     
-    s0 = 83.7
+    #s0 = 83.7
+    s0 = 1139.93
     x0 = np.log(s0) 
     res = np.zeros(nruns)
     print("Calculating cdf...")
@@ -34,9 +43,8 @@ def main_quadtree_one_strike():
     sigma = qt.calc_sigma(cdf_Ybar[1][0])*cdf_Ybar[0][0]
     for i in range(1, len(cdf_Ybar[0])):
             sigma += qt.calc_sigma(cdf_Ybar[1][i])*(cdf_Ybar[0][i]-cdf_Ybar[0][i-1])
-    print(sigma)
-    
-    '''for run in range(0,nruns):
+    sigma2 = np.zeros(nruns)
+    for run in range(0,nruns):
         sys.stdout.write("\rPerforming run " + str(run+1) + " of " + str(nruns))
         sys.stdout.flush()
 
@@ -48,8 +56,10 @@ def main_quadtree_one_strike():
     
     
     avg_sigma = np.mean(sigma2)
-    bs_price_avg_sigma = bsf.bs_call(s0, strike, 42/252, 0.0343, avg_sigma, 0)
-    bs_price = bsf.bs_call(s0, strike, 42/252, 0.0343, 0.234, 0)
+    #bs_price_avg_sigma = bsf.bs_call(s0, strike, 42/252, 0.0343, avg_sigma, 0)
+    #bs_price = bsf.bs_call(s0, strike, 42/252, 0.0343, 0.234, 0)
+    bs_price_avg_sigma = bsf.bs_call(s0, strike, 29/252, 0.01, avg_sigma, 0)
+    bs_price = bsf.bs_call(s0, strike, 29/252, 0.01, 0.13, 0)
 
     print("\n")
     print("Avg sigma ", sigma)
@@ -57,7 +67,7 @@ def main_quadtree_one_strike():
     print("Overall EV for strike = ", np.mean(res))
     print("BS Price with Avg Sigma = ", bs_price_avg_sigma)
     print("BS Price for assumptions = ", bs_price)
-    print()'''
+    print()
     return 0
 
 def main_quadtree_all_strikes():
