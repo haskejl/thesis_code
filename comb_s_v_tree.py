7a34d1679c78b0de5a017fb683e3942be595b76d
Joseph Haske <joseph.l.haske@gmail.com>
2022-04-13 20:56:16 +0300

    chaned bilinear interp implementation & refactored

--- comb_s_v_tree.py
+++ comb_s_v_tree.py
@@ -6,6 +6,32 @@ def calc_vn(y, v, k, theta, v_pos, dt, omega):
 def calc_zn(y, v, z, r, v_pos, dt):
     return(z+(r-0.5*v)*dt+y*np.sqrt(v_pos*dt))
 
+def bilinear_interp(X_arr, Y_arr, x_ind, y_ind, x, y, f):
+    x_ind = int(x_ind)
+    y_ind = int(y_ind)
+    if x_ind >= len(X_arr)-1:
+        if y_ind >= len(Y_arr)-1:
+            # Both on upper edge, no interpolation required
+            return f[x_ind, y_ind]
+        # X on upper edge, only interpolate Y
+        Cy = (y-Y_arr[y_ind])/(Y_arr[y_ind+1]-Y_arr[y_ind])
+        assert Cy >= 0 and Cy <= 1, "Linear Cy doesn't meet probability reqs, Cy =" + str(Cy)
+        return (1-Cy) * f[x_ind, y_ind] + Cy * f[x_ind, y_ind+1]
+
+    if y_ind >= len(Y_arr)-1:
+        # Y on upper edge, only interpolate X
+        Cx = (x-X_arr[x_ind])/(X_arr[x_ind+1]-X_arr[x_ind])
+        assert Cx >= 0 and Cx <= 1, "Linear Cx doesn't meet probability reqs, Cx =" + str(Cx)
+        return (1-Cx) * f[x_ind, y_ind] + Cx * f[x_ind+1, y_ind]
+    # Neither on upper edge, bilinear interpolation for both
+    Cx = (x-X_arr[x_ind])/(X_arr[x_ind+1]-X_arr[x_ind])
+    Cy = (y-Y_arr[y_ind])/(Y_arr[y_ind+1]-Y_arr[y_ind])
+    assert Cy >= 0 and Cy <= 1, "Bilinear Cy doesn't meet probability reqs, Cy =" + str(Cy)
+    assert Cx >= 0 and Cx <= 1, "Bilinear Cx doesn't meet probability reqs, Cx =" + str(Cx)
+
+    # Floating point error creeps in here
+    return (1-Cy)*((1-Cx)*f[x_ind,y_ind] + Cx*f[x_ind+1, y_ind]) + Cy*((1-Cx)*f[x_ind, y_ind+1] + Cx*f[x_ind+1, y_ind+1])
+
 def calc_sv_tree(n, mz, mv, Z0, V0):
     T = 0.25
     dt = T/n
@@ -56,91 +82,50 @@ def calc_sv_tree(n, mz, mv, Z0, V0):
     grid_z = np.linspace(z_tilde_min, z_tilde_max, mz, axis=-1)
     grid_v = np.linspace(v_tilde_min, v_tilde_max, mv, axis=-1)
     grid_s = np.zeros((n, mz, mv))
+    del(z_tilde_min)
+    del(z_tilde_max)
+    del(v_tilde_min)
+    del(v_tilde_max)
     
     # Calculate the final prices
-    grid_s[n-1,:,0] = np.exp(-r*T)*np.maximum(E-np.exp(grid_z[n-1,:]), 0)
+    grid_s[n-1,:,0] = np.maximum(E-np.exp(grid_z[n-1,:]), 0)
     
     # It seems to be that all values of v should have the same price for a given z
     #  fill them in here
     for i in range(1,len(grid_s[n-1,0])):
         grid_s[n-1,:,i] = grid_s[n-1,:,0]
 
+    Y_12 = np.array([-1,1])
     for k in range(n-2, -1, -1):
         # For each node in grid_s at time k
         for i in range(0, len(grid_z[k])):
             for j in range(0, len(grid_v[k])):
-                # Calculate the successor's coordinates
-                # i_1 and i_2 are coded for 0 = -1
-                # So v0 means y_1 = -1 and v1 means y_1 = 1
+                # Calculate the successors
                 v_pos = max(grid_v[k,j], 0)
-                z0 = calc_zn(-1, grid_v[k,j], grid_z[k,i], r, v_pos, dt)
-                z1 = calc_zn(1, grid_v[k,j], grid_z[k,i], r, v_pos, dt)
-                v0 = calc_vn(-1, grid_v[k,j], K, theta, v_pos, dt, omega)
-                v1 = calc_vn(1, grid_v[k,j], K, theta, v_pos, dt, omega)
-                if(z1 < z0):
-                    temp = z0
-                    z0 = z1
-                    z1 = temp
-                if(v1 < v0):
-                    temp = v0
-                    v0 = v1
-                    v1 = temp
-                # Find the successors values
-                #indices for the lower left corner
-                z0_i = int(np.floor((z0-grid_z[k+1,0])/dz[k+1]))
-                z1_i = int(np.floor((z1-grid_z[k+1,0])/dz[k+1]))
-                v0_i = int(np.floor((v0-grid_v[k+1,0])/dv[k+1]))
-                v1_i = int(np.floor((v1-grid_v[k+1,0])/dv[k+1]))
-                z1_i_1 = min(z1_i+1, mz-1)
-                v1_i_1 = min(v1_i+1, mv-1)
-                
-                def tilde_val(x, x0s, x1s): return (x-x0s)/(x1s-x0s)
-                
-                def c00(x_tilde, y_tilde): return (1-x_tilde)*(1-y_tilde)
-                def c10(x_tilde, y_tilde): return x_tilde*(1-y_tilde)
-                def c01(x_tilde, y_tilde): return (1-x_tilde)*y_tilde
-                def c11(x_tilde, y_tilde): return x_tilde*y_tilde
+                z_k1 = calc_zn(Y_12, grid_v[k,j], grid_z[k,i], r, v_pos, dt)
+                v_k1 = calc_vn(Y_12, grid_v[k,j], K, theta, v_pos, dt, omega)
 
-                z0_til = tilde_val(z0, grid_z[k+1,z0_i], grid_z[k+1,z0_i+1])
-                # If at the boundary use 1
-                z1_til = 1
-                if z1_i != z1_i_1:
-                   z1_til = tilde_val(z1, grid_z[k+1,z1_i], grid_z[k+1,z1_i_1])
-                v0_til = tilde_val(v0, grid_v[k+1,v0_i], grid_v[k+1,v0_i+1])
-                # If at the boundary use 1
-                v1_til = 1
-                if v1_i != v1_i_1:
-                    v1_til = tilde_val(v1, grid_v[k+1,v1_i], grid_v[k+1,v1_i_1])
+                assert z_k1[0] < z_k1[1], "z_k1[0] > z_k1[1], values:" + str(z_k1)
+                assert v_k1[0] < v_k1[1], "v_k1[0] > v_k1[1], values:" + str(v_k1)
 
-                # i_1 and i_2 are coded for 0 = -1
-                # So q0101 means i_1 = -1, i_2 = 1, i_3 = 0, i_4 = 1
-                q0000 = 0.25*(1+rho)*c00(v0_til, z0_til)*grid_s[k+1,z0_i,v0_i]
-                q0001 = 0.25*(1+rho)*c01(v0_til, z0_til)*grid_s[k+1,z0_i,v0_i+1]
-                q0010 = 0.25*(1+rho)*c10(v0_til, z0_til)*grid_s[k+1,z0_i+1,v0_i]
-                q0011 = 0.25*(1+rho)*c11(v0_til, z0_til)*grid_s[k+1,z0_i+1,v0_i+1]
+                # Find point to the lower left of the successor
+                z_ind = np.floor((z_k1-grid_z[k+1,0])/dz[k+1]).astype(int)
+                v_ind = np.floor((v_k1-grid_v[k+1,0])/dv[k+1]).astype(int)
 
-                q0100 = 0.25*(1-rho)*c00(v0_til, z1_til)*grid_s[k+1,z0_i,v1_i]
-                q0101 = 0.25*(1-rho)*c01(v0_til, z1_til)*grid_s[k+1,z0_i,v1_i_1]
-                q0110 = 0.25*(1-rho)*c10(v0_til, z1_til)*grid_s[k+1,z0_i+1,v1_i]
-                q0111 = 0.25*(1-rho)*c11(v0_til, z1_til)*grid_s[k+1,z0_i+1,v1_i_1]
+                interp_vals = np.zeros(4)
+                grid_s[k,i,j]
+                i_ind = 0
+                for ind in range(0,2):
+                    for jnd in range(0,2):
+                        interp_vals[i_ind] = bilinear_interp(grid_z[k+1], grid_v[k+1], z_ind[ind], v_ind[jnd], z_k1[ind], v_k1[jnd], grid_s[k+1])
+                        i_ind += 1
+                grid_s[k,i,j] = np.exp(-r*dt)*0.25 * ((1+rho)*(interp_vals[0]+interp_vals[3]) + (1-rho)*(interp_vals[1]+interp_vals[2]))
 
-                q1000 = 0.25*(1-rho)*c00(v1_til, z0_til)*grid_s[k+1,z1_i,v0_i]
-                q1001 = 0.25*(1-rho)*c01(v1_til, z0_til)*grid_s[k+1,z1_i,v0_i+1]
-                q1010 = 0.25*(1-rho)*c10(v1_til, z0_til)*grid_s[k+1,z1_i_1,v0_i]
-                q1011 = 0.25*(1-rho)*c11(v1_til, z0_til)*grid_s[k+1,z1_i_1,v0_i+1]
-
-                q1100 = 0.25*(1+rho)*c00(v1_til, z1_til)*grid_s[k+1,z1_i,v1_i]
-                q1101 = 0.25*(1+rho)*c01(v1_til, z1_til)*grid_s[k+1,z1_i,v1_i_1]
-                q1110 = 0.25*(1+rho)*c10(v1_til, z1_til)*grid_s[k+1,z1_i_1,v1_i]
-                q1111 = 0.25*(1+rho)*c11(v1_til, z1_til)*grid_s[k+1,z1_i_1,v1_i_1]
-
-                grid_s[k,i,j] = q0000+q0001+q0010+q0011+q0100+q0101+q0110+q0111+q1000+q1001+q1010+q1011+q1100+q1101+q1110+q1111
-
-    print(grid_s[0][0][0])
+    print(np.round(grid_s[0][0][0],4))
 
 if __name__ == "__main__":
     #calc_sv_tree(n, mz, mv, Z0, V0)
     calc_sv_tree(n=25, mz=125, mv=6, Z0=np.log(10), V0=0.25**2)
-    # Run result: 0.9819760855018189
-    #calc_sv_tree(n=71, mz=1000, mv=48, Z0=np.log(10), V0=0.25**2)
-    # Run result: 0.6123673190802092
+    # Run result: 0.4825
+    calc_sv_tree(n=71, mz=1000, mv=48, Z0=np.log(10), V0=0.25**2)
+    # Run result: 0.4954
