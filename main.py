1537337263a840d03e1082363a02223389901536
Joseph Haske <joseph.l.haske@gmail.com>
2022-04-24 18:15:42 +0300

    restored variable x_vals

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ def main_quadtree_one_strike(p_in):
     # 3 months of data
     #data = open("./data/IBM_2005_18May_to_18Jul.csv")
     # 1 year of data
-    data = open("./data/IBM_2004_18Jul_to_2005_18Jul.csv")
-    lines = data.readlines()
-    data.close()
+    #data = open("./data/IBM_2004_18Jul_to_2005_18Jul.csv")
+    #lines = data.readlines()
+    #data.close()
 
-    X_vals = np.zeros(len(lines))
-    for i in range(0,len(lines)):
-        X_vals[i] = np.log(float(lines[i].split(",")[1].strip()))
+    #X_vals = np.zeros(len(lines))
+    #for i in range(0,len(lines)):
+    #    X_vals[i] = np.log(float(lines[i].split(",")[1].strip()))
 
     #data = open("./data/sp500.csv")
     #lines = data.readlines()
@@ -30,9 +30,9 @@ def main_quadtree_one_strike(p_in):
     #for i in range(0,len(lines)):
     #    X_vals[i] = np.log(float(lines[i].strip()))
     #X_vals = np.log(np.array([78.38,78.21]))
-    #X_vals = np.log(np.array([76.36,77.16,76.41,76.51,75.81,76.0,77.14,77.1,75.55,76.84,77.35,75.79,75.0,75.04,74.8,
-    #    74.93,74.77,75.05,74.89,76.3,77.05,76.39,76.55,76.41,77.23,75.41,74.01,73.88,75.3,074.73,
-    #    74.2,74.67,74.79,75.81,77.38,79.3,78.96,80.04,81.45,82.42,82.38,81.81]))
+    X_vals = np.log(np.array([76.36,77.16,76.41,76.51,75.81,76.0,77.14,77.1,75.55,76.84,77.35,75.79,75.0,75.04,74.8,
+        74.93,74.77,75.05,74.89,76.3,77.05,76.39,76.55,76.41,77.23,75.41,74.01,73.88,75.3,074.73,
+        74.2,74.67,74.79,75.81,77.38,79.3,78.96,80.04,81.45,82.42,82.38,81.81]))
     
     s0 = 83.7
     #s0 = 1139.93
@@ -42,7 +42,8 @@ def main_quadtree_one_strike(p_in):
     cdf_Ybar = est.calc_cdf(X_vals)
     sigma = qt.calc_sigma(cdf_Ybar[1][0])*cdf_Ybar[0][0]
     for i in range(1, len(cdf_Ybar[0])):
-            sigma += qt.calc_sigma(cdf_Ybar[1][i])*(cdf_Ybar[0][i]-cdf_Ybar[0][i-1])
+        sigma += qt.calc_sigma(cdf_Ybar[1][i])*(cdf_Ybar[0][i]-cdf_Ybar[0][i-1])
+    print(sigma)
     sigma2 = np.zeros(nruns)
     for run in range(0,nruns):
         sys.stdout.write("\rPerforming run " + str(run+1) + " of " + str(nruns))
@@ -50,7 +51,7 @@ def main_quadtree_one_strike(p_in):
 
         Y_bar = est.use_cdf(cdf_Ybar[0], cdf_Ybar[1], N)
         # Use the line below to simulate constant volatility on the tree
-        #Y_bar = np.array([np.log(0.234)], N)
+        #Y_bar = np.repeat([np.log(0.234)], N)
         sigma2[run] = np.mean(qt.calc_sigma(Y_bar))
         res[run] = qt.calc_quad_tree_ev(x0, Y_bar, N, strike, qt.payoff_func_call, p)
     
